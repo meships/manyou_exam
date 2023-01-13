08672497e67c7402fa553461e066d293924e6871
qz <meshipsmou@yahoo.co.jp>
2023-01-13 11:48:59 +0900

    limitカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_09_144021) do
+ActiveRecord::Schema.define(version: 2023_01_13_022425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_01_09_144021) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "limit", null: false
   end
 
 end
