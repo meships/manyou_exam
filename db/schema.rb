19a9ec8bf713b46821ff3d731e72d51e18fd8eb8
qz <meshipsmou@yahoo.co.jp>
2023-01-14 23:21:40 +0900

    終了期限でソートする、ステータスカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_13_022425) do
+ActiveRecord::Schema.define(version: 2023_01_14_140450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_01_13_022425) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "limit", null: false
+    t.string "status"
   end
 
 end
