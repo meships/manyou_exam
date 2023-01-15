2f516b92de58ba8896326fa09746f3e7007e3f0f
qz <meshipsmou@yahoo.co.jp>
2023-01-15 22:50:09 +0900

    ステータスと優先度追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :title)
+    params.require(:task).permit(:content, :title, :limit, :stasus, :priority)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_140450) do
+ActiveRecord::Schema.define(version: 2023_01_14_145626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2023_01_14_140450) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "limit", null: false
     t.string "status"
+    t.integer "priority"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
